f91a4343c2893d059b625c7448a37d0b78be22cb
João Gabriel da Silva <55601337+gabrielsilvadev@users.noreply.github.com>
2020-10-08 14:13:04 -0300

    Create linha da matriz.py

---
+++ linha da matriz.py
@@ -0,0 +1,21 @@
+l=int(input())
+opera=input()
+m=[]
+for i in range(12):
+       m.append([])
+
+for i in  range(12):
+    for j in range(12):
+        x=float(input())
+        m[i].append(x)
+if opera=='S':
+    soma=0
+    for k in m[l]:
+        soma=soma+k
+    print(soma)
+if opera=='M':
+    soma=0
+    for k in m[l]:
+        soma=soma+k
+    med=soma/12
+    print("{:.1f}".format(med))
